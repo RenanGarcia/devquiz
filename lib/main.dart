09b167243fb328becfe20807d4753021f1f112f4
Renan Nobre Garcia <renan.nobregarcia@gmail.com>
2021-04-19 22:48:43 -0300

    Created splash page

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppWidget();
+  }
+}
